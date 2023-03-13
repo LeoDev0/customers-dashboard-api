2cb90e5a18c0210a3646e25a58ea6d8069fbc0a8
Leonardo T. Fernandes <leo.80@protonmail.com>
2023-03-13 00:08:32 -0300

    feat: add fake data to database and implement selectCustomerById method

--- src/main/java/com/leodev0/Main.java
+++ src/main/java/com/leodev0/Main.java
@@ -1,10 +1,15 @@
 package com.leodev0;
 
+import com.leodev0.customer.Customer;
+import com.leodev0.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -22,5 +27,24 @@ public class Main {
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println(beanDefinitionName);
         }
+    }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            Customer alex = new Customer(
+                    "Alex",
+                    "alex@gmail.com",
+                    21
+            );
+
+            Customer jamila = new Customer(
+                    "Jamila",
+                    "jamila@gmail.com",
+                    19
+            );
+
+            customerRepository.saveAll(List.of(alex, jamila));
+        };
     }
 }
